872cb9a0f000d21fbe4e11350e3356d4d09fac4c
anaghasunkadh <Anaghasrinat@gmail.com>
2025-07-23 21:10:53 -0400

    change

--- src/Kambaz/Database/index.ts
+++ src/Kambaz/Database/index.ts
@@ -1,10 +1,7 @@
-// src/Kambaz/Database/index.ts
-import coursesRaw from "./courses.json?raw";
-import modulesRaw from "./modules.json?raw";
-import assignmentsRaw from "./assignments.json?raw";
-import usersRaw from "./users.json?raw";
 
-export const courses = JSON.parse(coursesRaw);
-export const modules = JSON.parse(modulesRaw);
-export const assignments = JSON.parse(assignmentsRaw);
-export const users = JSON.parse(usersRaw);
+import courses from "./courses.json";
+import modules from "./modules.json";
+import assignments from "./assignments.json";
+import users from "./users.json";
+
+export { courses, modules, assignments, users };
