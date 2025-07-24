9774ff665162b52f72ab9b99e44c4750527bc737
anaghasunkadh <Anaghasrinat@gmail.com>
2025-07-23 20:50:19 -0400

    more changes

--- src/Kambaz/Courses/index.tsx
+++ src/Kambaz/Courses/index.tsx
@@ -1,44 +1,42 @@
-import CoursesNavigation from "./Navigation";
+import CourseNavigation from "./Navigation";
+import { Navigate, Route, Routes,useParams, useLocation } from "react-router";
 import Modules from "./Modules";
 import Home from "./Home";
-import { courses } from "../Database";
-import { Navigate, Route, Routes, useLocation, useParams } from "react-router";
-import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/Editor";
+import Assignments from "./Assignments";
 import { FaAlignJustify } from "react-icons/fa";
 import PeopleTable from "./People/Table";
-
+import { courses } from "../Database";
 export default function Courses() {
-  const { cid } = useParams();
-  const course = courses.find((course) => course._id === cid);
-  const { pathname } = useLocation();
+    const { cid } = useParams();
+    const course = courses.find((course) => course._id === cid);
+    const { pathname } = useLocation();
+    return (
+        <div id="wd-courses" style={{ padding: '32px' }}>
+            <h2 className="text-danger">
+                <FaAlignJustify className="me-4 fs-4 mb-1" />
+                {course ? course.name : `Course ${cid}`} &gt; {pathname.split("/")[4]}
+            </h2>
+            <hr />
+            <div className="d-flex">
+                <div className="d-none d-md-block">
+                    <CourseNavigation />
+                </div>
+                <div className="flex-fill">
+                    <Routes>
+                        <Route path="/" element={<Navigate to="Home" />} />
+                        <Route path="Home" element={<Home />} />
+                        <Route path="Modules" element={<Modules />} />
+                        <Route path="Piazza" element={<h2>Piazza</h2>} />
+                        <Route path="Zoom" element={<h2>Zoom</h2>} />
+                        <Route path="Quizzes" element={<h2>Quizzes</h2>} />
+                        <Route path="Assignments" element={<Assignments />} />
+                        <Route path="Grades" element={<h2>Grades</h2>} />
+                        <Route path="Assignments/:aid" element={<AssignmentEditor />} />
+                        <Route path="People" element={<PeopleTable />} />
 
-  return (
-    <div id="wd-courses">
-      <h2 className="text-danger"><FaAlignJustify className="me-4 fs-4 mb-1" />
-      {course && course.name}  &gt; {pathname.split("/")[4]}
-      </h2>
-      <hr />
-      <div className="d-flex">
-    <div className="d-none d-md-block">
-    </div>
-
-            <CoursesNavigation />
-            <div className="flex-fill">
-
-            <Routes>
-              <Route path="/"
-                     element={<Navigate to="Home" />} />
-              <Route path="Home" element={<Home />} />
-              <Route path="Modules" element={<Modules />} />
-              <Route path="Assignments"
-                     element={<Assignments/>} />
-              <Route path="Assignments/:aid"
-                     element={<AssignmentEditor/>} />
-              <Route path="People" element={<PeopleTable />} />
-            </Routes>
-            </div></div>
-
-    </div>
-);
+                    </Routes>
+                </div></div>
+        </div>
+    );
 }
